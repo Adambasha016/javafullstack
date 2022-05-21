a9e630d307b7ec7b769e54dbadff14bce7560b5e
Adambasha016 <adambasha016@gmail.com>
2022-05-21 20:03:21 +0530

    my first commit

---
+++ javafullstack/adam/A.java
@@ -0,0 +1,17 @@
+	class F
+	{
+	   public static void main m3()
+		{
+		   System.out. println("m3 of classF);
+
+	}
+	}
+	class G
+	{
+	   public static void main(String[] args)
+		{
+		   System.out.println("main starts");
+		   F.m();
+		  System.out.println("nain ends");
+		}
+	}
